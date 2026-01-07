ea824279395bdf6cff4a5a137f961392ee8512ea
Hippl227 <ashervaheed227@gmail.com>
2026-01-07 09:26:04 +0000

    Fix syntax errors and improve message handling

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 import discord
-class Client(discord.Client:
-  async def on_ready(self):
-    print(f'Logged in as {self.user} (ID: {self.user. id})')
-    
-  async def on_message self, message):
-    print f'Message from {message.author}: (message.content}')
-    if message.content.startswith ('!hello'):
-      await message.channel.send('hello')
-    
-intents= daicord.Intents.default()
-intents.message_content = true
+class Client(discord.Client):
+    async def on_ready(self):
+       print(f'Logged in as {self.user} (ID: {self.user.id})')
 
-client=Client(intents=intents)
-client. run('MTQ10DE4MjExNTMONTgy0TkOMQ.GZxL60.sTw_1PHFw5Jl1NxnWZ5-kTu4LB4vpdu19UY7rE')
+    async def on_message(self, message):
+        print(f'Message from {message.author}: {message.content}')
+        if message.content.startswith('!hello'):
+            await message.channel.send('Hello!')
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = Client(intents=intents)
+client.run('MTQ1ODE4MjExNTM0NTgyOTk0MQ.GZxL6o.sTw_1PHFw5Jl1NxnWZ5-kTu4LB4vpdu19UY7rE')
